99d1fb9e7d7b4431b5016f6a473bde49b03fd91e
Agoraph0bia <jhoward132@gmail.com>
2024-12-03 09:32:36 -0800

    Sidepanel update

--- src/sidepanel/sidepanel.ts
+++ src/sidepanel/sidepanel.ts
@@ -1,37 +1,19 @@
-let creating: any;
-let offscreenUrl = '../src/inject/elementpicker.html';
-
 document
-	.getElementById('element-picker-button')
-	?.addEventListener('click', async () =>
-		closeExistingContext().then(createElementPicker).then(closeSidePanel)
-	);
+  .getElementById('element-picker-button')
+  ?.addEventListener('click', async () =>
+    createElementPicker().then(closeSidePanel)
+  );
 
 async function createElementPicker() {
-	if (!creating) {
-		creating = chrome.offscreen.createDocument({
-			url: offscreenUrl,
-			reasons: [
-				chrome.offscreen.Reason.IFRAME_SCRIPTING,
-				chrome.offscreen.Reason.DOM_SCRAPING,
-			],
-			justification:
-				'Overlay semi-transparent document on top of current document and capture mouse events',
-		});
-		await creating;
-		creating = null;
-	}
-}
-
-async function closeExistingContext() {
-	const existingContexts = await chrome.runtime.getContexts({
-		contextTypes: [chrome.runtime.ContextType.OFFSCREEN_DOCUMENT],
-		documentUrls: [offscreenUrl],
-	});
-
-	existingContexts?.forEach((c) => chrome.offscreen.closeDocument());
+  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id ?? 0, {
+      ext: 'Styl',
+      type: 'elementpicker',
+      action: 'start',
+    });
+  });
 }
 
 async function closeSidePanel() {
-	window.close();
+  window.close();
 }

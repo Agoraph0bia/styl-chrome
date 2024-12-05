a454070e293d8ddf70439874628e36ed52ded004
Agoraph0bia <jhoward132@gmail.com>
2024-12-05 13:21:30 -0800

    Moving messaging logic to content script/iframe

--- src/sidepanel/sidepanel.ts
+++ src/sidepanel/sidepanel.ts
@@ -1,22 +1,24 @@
 document
-	.getElementById('element-picker-button')
-	?.addEventListener('click', async () =>
-		createElementPicker().then(closeSidePanel)
-	);
+  .getElementById('element-picker-button')
+  ?.addEventListener('click', async () =>
+    createElementPicker().then(closeSidePanel)
+  );
 
 async function createElementPicker() {
-	let tabs = await chrome.tabs.query({
-		highlighted: true,
-		currentWindow: true,
-	});
+  let tabs = await chrome.tabs.query({
+    highlighted: true,
+    currentWindow: true,
+  });
 
-	chrome.tabs.sendMessage(tabs[0].id ?? 0, {
-		type: 'elementpicker',
-		action: 'start',
-		extId: chrome.runtime.id,
-	});
+  if (!tabs[0].id) return;
+
+  chrome.tabs.sendMessage(tabs[0].id, {
+    type: 'elementpicker',
+    action: 'start',
+    extId: chrome.runtime.id,
+  });
 }
 
 async function closeSidePanel() {
-	window.close();
+  window.close();
 }
